c62f98de02bf280596fca27a8712f1e5ec6a1968
Yuma Shinohara <shinohara_yuma@cyberagent.co.jp>
2023-06-11 15:37:02 +0900

    Make gpt read the contents of the paper and display all paper pages on one slide

--- mkmd.py
+++ mkmd.py
@@ -1,123 +1,116 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import os
 import io
-import sys
-import time
-import arxiv
-import openai
-import random
-import fitz
-from xml.dom import minidom
-import xmltodict
-import dicttoxml
-import json
+import operator
+import argparse
+import datetime
 import glob
+
+import fitz
 import xmltodict
 from PIL import Image
+from tqdm import tqdm
 
-print(fitz.__doc__)
+if not tuple(map(int, fitz.version[0].split('.'))) >= (1, 18, 18):
+    raise SystemExit('require PyMuPDF v1.18.18+')
 
-if not tuple(map(int, fitz.version[0].split("."))) >= (1, 18, 18):
-    raise SystemExit("require PyMuPDF v1.18.18+")
+# This key is the item name when writing to markdown (can be set arbitrarily),
+# and value must match the name of the key saved in xml (need to be adjusted according to the prompt to GPT).
+XML_DICT_KEYS = {'要旨': 'summary',
+                 '入出力': 'input_output',
+                 '新規性・手法のキモ': 'method',
+                 '検証方法': 'validation',
+                 '議論': 'discussion'}
 
-def recoverpix(doc, item):
+
+def recover_pix(doc, item):
     xref = item[0]  # xref of PDF image
-    smask = item[1]  # xref of its /SMask
+    s_mask = item[1]  # xref of its /SMask
 
-    # special case: /SMask or /Mask exists
-    if smask > 0:
-        pix0 = fitz.Pixmap(doc.extract_image(xref)["image"])
-        if pix0.alpha:  # catch irregular situation
+    # Special case: /SMask or /Mask exists
+    if s_mask > 0:
+        pix0 = fitz.Pixmap(doc.extract_image(xref)['image'])
+        if pix0.alpha:  # Catch irregular situation
             pix0 = fitz.Pixmap(pix0, 0)  # remove alpha channel
-        mask = fitz.Pixmap(doc.extract_image(smask)["image"])
+        mask = fitz.Pixmap(doc.extract_image(s_mask)['image'])
 
         try:
             pix = fitz.Pixmap(pix0, mask)
-        except:  # fallback to original base image in case of problems
-            pix = fitz.Pixmap(doc.extract_image(xref)["image"])
+        except:
+            # Fallback to original base image in case of problems
+            pix = fitz.Pixmap(doc.extract_image(xref)['image'])
 
         if pix0.n > 3:
-            ext = "pam"
+            ext = 'pam'
         else:
-            ext = "png"
+            ext = 'png'
 
-        return {  # create dictionary expected by caller
-            "ext": ext,
-            "colorspace": pix.colorspace.n,
-            "image": pix.tobytes(ext),
+        # Create dictionary expected by caller
+        return {
+            'ext': ext,
+            'colorspace': pix.colorspace.n,
+            'image': pix.tobytes(ext),
         }
 
-    # special case: /ColorSpace definition exists
-    # to be sure, we convert these cases to RGB PNG images
-    if "/ColorSpace" in doc.xref_object(xref, compressed=True):
+    # Special case: /ColorSpace definition exists
+    # To be sure, we convert these cases to RGB PNG images
+    if '/ColorSpace' in doc.xref_object(xref, compressed=True):
         pix = fitz.Pixmap(doc, xref)
         pix = fitz.Pixmap(fitz.csRGB, pix)
-        return {  # create dictionary expected by caller
-            "ext": "png",
-            "colorspace": 3,
-            "image": pix.tobytes("png"),
+
+        # Create dictionary expected by caller
+        return {
+            'ext': 'png',
+            'colorspace': 3,
+            'image': pix.tobytes('png'),
         }
     return doc.extract_image(xref)
 
 
-def extract_images_from_pdf(fname, imgdir="./output", min_width=400, min_height=400, relsize=0.05, abssize=2048, max_ratio=8, max_num=5):
-    '''
-    dimlimit = 0  # 100  # each image side must be greater than this
-    relsize = 0  # 0.05  # image : image size ratio must be larger than this (5%)
-    abssize = 0  # 2048  # absolute image size limit 2 KB: ignore if smaller
-    '''
+def extract_images_from_pdf(f_name, imgdir='./output', min_width=400, min_height=400, max_img_file_size=2048,
+                            max_ratio=8, max_num=5):
+    os.makedirs(imgdir, exist_ok=True)
 
-    if not os.path.exists(imgdir):  # make subfolder if necessary
-        os.mkdir(imgdir)
+    doc = fitz.open(f_name)
 
-    t0 = time.time()
-    doc = fitz.open(fname)
-    page_count = doc.page_count  # number of pages
-
-    xreflist = []
-    imglist = []
+    xref_list = []
+    img_list = []
     images = []
-    for pno in range(page_count):
+    for pno in range(doc.page_count):
         if len(images) >= max_num:
             break
-        print(f"extract images {pno+1}/{page_count}")
         il = doc.get_page_images(pno)
-        imglist.extend([x[0] for x in il])
+        img_list.extend([x[0] for x in il])
         for img in il:
             xref = img[0]
-            if xref in xreflist:
+            if xref in xref_list:
                 continue
             width = img[2]
             height = img[3]
-            print(f"{width}x{height}")
             if width < min_width and height < min_height:
                 continue
-            image = recoverpix(doc, img)
-            n = image["colorspace"]
-            imgdata = image["image"]
+            image = recover_pix(doc, img)
+            n = image['colorspace']
+            imgdata = image['image']
 
-            if len(imgdata) <= abssize:
+            if len(imgdata) <= max_img_file_size:
                 continue
 
-            if width / height > max_ratio or height/width > max_ratio:
-                print(f"max_ration {width/height} {height/width} {max_ratio}")
+            if width / height > max_ratio or height / width > max_ratio:
                 continue
 
-            print("*")
+            img_name = 'img%02d_%05i.%s' % (pno + 1, xref, image['ext'])
+            images.append((img_name, pno + 1, width, height))
+            img_file = os.path.join(imgdir, img_name)
+            with open(img_file, 'wb') as f:
+                f.write(imgdata)
+            xref_list.append(xref)
 
-            imgname = "img%02d_%05i.%s" % (pno+1, xref, image["ext"])
-            images.append((imgname, pno+1, width, height))
-            imgfile = os.path.join(imgdir,  imgname)
-            fout = open(imgfile, "wb")
-            fout.write(imgdata)
-            fout.close()
-            xreflist.append(xref)
+    img_list = list(set(img_list))
+    return xref_list, img_list, images
 
-    t1 = time.time()
-    imglist = list(set(imglist))
-    print(len(set(imglist)), "images in total")
-    print(len(xreflist), "images extracted")
-    print("total time %g sec" % (t1 - t0))
-    return xreflist, imglist, images
 
 def get_half(fname):
     # Open the PDF file
@@ -139,78 +132,179 @@ def get_half(fname):
     return im_cropped
 
 
+def get_concat_h_multi_resize(im_list, resample=Image.BICUBIC):
+    min_height = min(im.height for im in im_list)
+    im_list_resize = [im.resize((int(im.width * min_height / im.height), min_height), resample=resample)
+                      for im in im_list]
+    total_width = sum(im.width for im in im_list_resize)
+    dst = Image.new('RGB', (total_width, min_height))
+    pos_x = 0
+    for im in im_list_resize:
+        dst.paste(im, (pos_x, 0))
+        pos_x += im.width
+    return dst
 
-def make_md(f, dirname, filename, nimages=3, keywords=[]):
-    path = f"{dirname}/{filename}"
-    with open(path, "r") as fin:
+
+def get_concat_v_multi_resize(im_list, resample=Image.BICUBIC):
+    min_width = min(im.width for im in im_list)
+    im_list_resize = [im.resize((min_width, int(im.height * min_width / im.width)), resample=resample)
+                      for im in im_list]
+    total_height = sum(im.height for im in im_list_resize)
+    dst = Image.new('RGB', (min_width, total_height))
+    pos_y = 0
+    for im in im_list_resize:
+        dst.paste(im, (0, pos_y))
+        pos_y += im.height
+    return dst
+
+
+def get_concat_tile_resize(im_list_2d, resample=Image.BICUBIC):
+    im_list_v = [get_concat_h_multi_resize(im_list_h, resample=resample) for im_list_h in im_list_2d]
+    return get_concat_v_multi_resize(im_list_v, resample=resample)
+
+
+def get_all_page(fname, max_w=1600, max_h=900):
+    pdf_file = fitz.open(fname)
+    page_imgs = []
+    for page in pdf_file:
+        try:
+            mat = fitz.Matrix(2, 2)  # zoom factor 2 in each direction
+            # Get the page as a whole image
+            pix = page.get_pixmap(matrix=mat)
+            # Convert to a PIL Image
+            im = Image.open(io.BytesIO(pix.tobytes()))
+            page_imgs.append(im)
+        except Exception as e:
+            print(e)
+            continue
+
+    if pdf_file.page_count <= 8:
+        n = int(pdf_file.page_count / 2)
+        top_imgs = page_imgs[:n]
+        bottom_imgs = page_imgs[n:]
+        tiled_img = get_concat_tile_resize([top_imgs, bottom_imgs])
+    else:
+        n = int(pdf_file.page_count / 3)
+        top_imgs = page_imgs[:n]
+        middle_imgs = page_imgs[n:2 * n]
+        bottom_imgs = page_imgs[2 * n:]
+        tiled_img = get_concat_tile_resize([top_imgs, middle_imgs, bottom_imgs])
+
+    w, h = tiled_img.size
+    if w < h:
+        tiled_img = tiled_img.resize((max_w, int(max_w * h / w)))
+    else:
+        tiled_img = tiled_img.resize((int(max_h * w / h), max_h))
+
+    return tiled_img
+
+
+def make_md(f, dirname, filename, keywords=[]):
+    path = f'{dirname}/{filename}'
+    with open(path, 'r') as fin:
         xml = fin.read()
         xml_lower = xml.lower()
-        if (keywords is not None) and not(any([k.lower() in xml_lower for k in keywords])):
+        if (keywords is not None) and not (any([k.lower() in xml_lower for k in keywords])):
             return
-    dict = xmltodict.parse(xml)['paper']
-    print(dict)
-    f.write('\n---\n')
-    f.write('<!-- _class: title -->\n')
-    f.write(f"# {dict['title_jp']}\n")
-    f.write(f"{dict['title']}\n")
-    #authors = ",".join(dict['authors']['item'])
-    #f.write(f"{authors}\n")
-    f.write(f"[{dict['year']}] {dict['keywords']} {dict['entry_id']}\n") 
-    f.write(f"__課題__ {dict['problem']}\n")
-    f.write(f"__手法__ {dict['method']}\n")
-    f.write(f"__結果__ {dict['result']}\n")
+    paper_info = xmltodict.parse(xml)['paper']
 
-    pdfname = f"{dirname}/paper.pdf"
-    img_cropped = get_half(pdfname)
-    img_cropped.save(f"{dirname}/half.png", "PNG")
-    
-    f.write("\n---\n")
-    f.write('<!-- _class: info -->\n') 
+    # Get half top image of pdf
+    pdf_name = f'{dirname}/paper.pdf'
+    img_cropped = get_half(pdf_name)
+    img_cropped.save(f'{dirname}/half.png', 'PNG')
+
+    f.write('\n---\n')
+    f.write('<!-- _class: info -->\n')
     f.write(f'![width:1400]({dirname}/half.png)\n')
 
-    # get images
-    _, _, image_list = extract_images_from_pdf(pdfname, imgdir=dirname)
-    images = [{'src':imgname, 'pno':str(pno), 'width':str(width), 'height':str(height)} for imgname, pno, width, height in image_list]
-    for img in images[:nimages]:
+    # Get tiled image of pdf
+    tiled_pages = get_all_page(pdf_name)
+    tiled_pages.save(f'{dirname}/tiled_pages.jpg', 'JPEG')
+
+    f.write('\n---\n')
+    f.write('<!-- _class: info -->\n')
+    f.write(f'![height:650]({dirname}/tiled_pages.jpg)\n')
+
+    # Get all images from the pdf
+    _, _, image_list = extract_images_from_pdf(pdf_name, imgdir=dirname)
+    images = [{'src': imgname, 'pno': pno, 'width': width, 'height': height} for
+              imgname, pno, width, height in image_list]
+
+    # Get 1st & 2nd images in the pdf
+    images = sorted(images, key=operator.itemgetter('pno'))[:2]
+
+    # Write the images
+    for img in images:
         src = img['src']
-        width = (int)(img['width'])
-        height = (int)(img['height'])
-        print("#### img", src, width, height)
+        width = int(img['width'])
+        height = int(img['height'])
         x_ratio = (1600.0 * 0.7) / (float)(width)
         y_ratio = (900.0 * 0.7) / (float)(height)
         ratio = min(x_ratio, y_ratio)
 
-        f.write("\n---\n")
-        f.write('<!-- _class: info -->\n') 
+        f.write('\n---\n')
+        f.write('<!-- _class: info -->\n')
         f.write(f'![width:{(int)(ratio * width)}]({dirname}/{src})\n')
 
-def main(dir="./xmrs", output="./out.md", keywords=[]):
-    print("### dir", dir, "output", output, "keywords", keywords)
-    xmlfiles= glob.glob(f"{dir}/*/*.xml")
-    with open(output, "w") as f:
-        f.write("---\n")
-        f.write("marp: true\n")
-        f.write("theme: default\n")
-        f.write("size: 16:9\n")
-        f.write("paginate: true\n")
+    # Write summaries
+    f.write('\n---\n')
+    f.write('<!-- _class: title -->\n')
+    f.write(f"## {paper_info['title']}\n")
+
+    f.write(f"[{paper_info['date']}] {paper_info['entry_id']}\n")
+    try:
+        f.write(f"__Keywords__ {paper_info['keywords']}\n")
+    except:
+        pass
+
+    for i, (k, v) in enumerate(XML_DICT_KEYS.items()):
+        try:
+            # New page when "i" is odd to make it easier to see as slides
+            if i % 2 == 1:
+                f.write('\n---\n')
+
+            f.write(f"\n__{k}__\n{paper_info[v]}\n")
+        except KeyError as e:
+            print(e)
+            f.write(f"__{v}__\nNone\n")
+
+
+def get_files(xml_dir):
+    xml_files = glob.glob(f'{xml_dir}/*/*.xml')
+
+    # Ascending order by publish date
+    xml_files.sort()
+    return xml_files
+
+
+def main(input_dir='./xmrs', output='./out.md', keywords=[]):
+    xml_files = get_files(xml_dir=input_dir)
+    dt_now = datetime.datetime.now()
+    date = dt_now.strftime('%Y年%m月%d日')
+    with open(output, 'w') as f:
+        f.write('---\n')
+        f.write('marp: true\n')
+        f.write('theme: default\n')
+        f.write('size: 16:9\n')
+        f.write('paginate: true\n')
         f.write('_class: ["cool-theme"]\n')
         f.write('\n---\n')
         f.write(f'# {keywords} on arXiv\n')
-        f.write('automatically generated by ChatGPT\n')
+        f.write('Generated by GPT-4\n')
+        f.write(f'{date}\n')
         f.write('\n')
 
-        for file in xmlfiles:
+        for file in tqdm(xml_files):
             dirname, filename = os.path.split(file)
-            print(dirname, filename)
+            tqdm.write(file)
             make_md(f, dirname, filename, keywords=keywords)
-    print("### result stored in", output)
+    print('Output file: ', output)
 
-import argparse
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dir', "-d", type=str, help='xml dir', default='./xmls')
-    parser.add_argument('--output', "-o", type=str, default='output.md', help='output markdown file')
+    parser.add_argument('--dir', '-d', type=str, help='xml dir', default='./xmls')
+    parser.add_argument('--output', '-o', type=str, default='output.md', help='output markdown file')
     parser.add_argument('positional_args', nargs='?', help='query keywords')
     args = parser.parse_args()
 
@@ -219,5 +313,5 @@ if __name__ == "__main__":
         keywords = [keywords]
 
     print(args, keywords)
-    
-    main(dir=args.dir, output=args.output, keywords=keywords)
+
+    main(input_dir=args.dir, output=args.output, keywords=keywords)
--- query_arxiv.py
+++ query_arxiv.py
@@ -1,136 +1,179 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import os
-import io
 import sys
-import time
-import arxiv
-import openai
+import argparse
 import random
+
 from xml.dom import minidom
-import xmltodict
 import dicttoxml
-import json
+from tqdm import tqdm
+import arxiv
 
-#OpenAIのapiキー
-openai.api_key = 'your openai key'
+import openai
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader
 
-prompt = """与えられた論文の要点をまとめ、以下の項目で日本語で出力せよ。それぞれの項目は最大でも180文字以内に要約せよ。
+PROMPT = """以下の項目を出力せよ。翻訳が難しい技術用語については、日本語ではなく英単語を使用しても構わない。
 ```
-論文名:タイトルの日本語訳
-キーワード:この論文のキーワード
-課題:この論文が解決する課題
-手法:この論文が提案する手法
-結果:提案手法によって得られた結果
+キーワード:この論文のキーワードを最大5つ（この項目は英語で出力せよ）
+要旨:論文の概要。どんなもの？課題は何？批判されている理論は何？なぜそれが問題なのか？どうそれを改善したのか？（日本語で200字程度）
+入出力:入力と出力の例を示せ。入力はどんなもの？出力はどんなもの？画像であれば画像サイズやフレーム数など具体的に述べよ。（日本語で200字以内）
+新規性・手法のキモ:この論文が提案する手法の核心となるアイデアと、具体的な説明（日本語で200字程度）
+検証方法:どうやって提案アイディアが有効であることを示したのか？データセットや評価指標はなにか？具体的にどうやって評価したのか？結果はどうだったのか？（日本語で200字以内）
+議論:検証結果から何が言えるか？逆に検証結果から言えないことは何か？今後の課題は何か？対象となるスコープにおいて網羅性と整合性はあるか？発展的な課題はあるか？（日本語で200字以内）
 ```"""
 
-def get_summary(result):
-    text = f"title: {result.title}\nbody: {result.summary}"
-    print("### input text", text)
-    #print("### input prompt", prompt)
-    response = openai.ChatCompletion.create(
-                #model="gpt-3.5-turbo",
-                model='gpt-4',
-                messages=[
-                    {'role': 'system', 'content': prompt},
-                    {'role': 'user', 'content': text}
-                ],
-                temperature=0.25,
-            )
-    summary = response['choices'][0]['message']['content']
-    print("#### GPT", summary)
-    dict = {}    
+MODEL_NAME = 'gpt-4'
+# MODEL_NAME = 'gpt-3.5-turbo'
+
+# DOWNLOAD_PDF = False  # False if you have already downloaded the PDF
+DOWNLOAD_PDF = True
+
+TEMPERATURE = 0.0
+
+
+def get_gpt_summary(paper_info):
+    print('Summarizing paper: ', paper_info['title'])
+
+    # Create vector store
+    embeddings = OpenAIEmbeddings()
+    db = Chroma.from_documents(paper_info['pages'], embedding=embeddings)
+    # Create retriever
+    retriever = db.as_retriever()
+
+    # Create chat model
+    llm = ChatOpenAI(temperature=TEMPERATURE, model_name=MODEL_NAME, openai_api_key=openai.api_key)
+    crc = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, return_source_documents=True)
+    query = PROMPT
+    chat_history = []
+
+    # Ask question
+    print('Waiting for response from OpenAI server...')
+    # ToDo: Sometimes, the response ended up with mid-sentence, so need to deal with it.
+    response = crc({'question': query, 'chat_history': chat_history})
+
+    print('Response received.')
+    summary = response['answer']
+
+    summary_dict = {}
     for b in summary.split('\n'):
-        print("****", b)
-        if b.startswith("論文名"):
-            dict['title_jp'] = b[4:].lstrip()
-        if b.startswith("キーワード"):
-            dict['keywords'] = b[6:].lstrip()
-        if b.startswith("課題"):
-            dict['problem'] = b[3:].lstrip()
-        if b.startswith("手法"):
-            dict['method'] = b[3:].lstrip()
-        if b.startswith("結果"):
-            dict['result'] = b[3:].lstrip()
-    print("Dict by ChatGPT", dict)
-    return dict
+        print('****', b)
+        if b.startswith('キーワード'):
+            summary_dict['keywords'] = b[6:].lstrip()
+        elif b.startswith('要旨'):
+            summary_dict['summary'] = b[3:].lstrip()
+        elif b.startswith('入出力'):
+            summary_dict['input_output'] = b[4:].lstrip()
+        elif b.startswith('新規性・手法のキモ'):
+            summary_dict['method'] = b[10:].lstrip()
+        elif b.startswith('検証方法'):
+            summary_dict['validation'] = b[5:].lstrip()
+        elif b.startswith('議論'):
+            summary_dict['discussion'] = b[3:].lstrip()
+    print('Dict by GPT', summary_dict)
+    return summary_dict
 
-def get_paper_info(result, dirpath="./xmls"):
-    dict = {}
-    dict['title']= result.title
-    dict['date'] = result.published.strftime("%Y-%m-%d %H:%M:%S")
-    dict['authors'] = [x.name for x in result.authors]
-    dict['year'] = str(result.published.year)
-    dict['entry_id'] = str(result.entry_id)
-    dict['primary_category'] = str(result.primary_category)
-    dict['categories'] = result.categories
-    dict['journal_ref'] = str(result.journal_ref)
-    dict['pdf_url'] = str(result.pdf_url)
-    dict['doi']= str(result.doi)
-    dict['abstract'] = str(result.summary)
-                
-    print("##### DIR", dirpath, "PDF", result.pdf_url, "DOI", result.doi, "ID", id)
-    if not os.path.exists(dirpath):
-        os.mkdir(dirpath)
 
-    # download paper PDF"
-    print("download", f"{dirpath}/paper.pdf")
-    result.download_pdf(dirpath=dirpath,filename="paper.pdf")
-    dict['pdf'] = 'paper.pdf'
+def get_paper_info(result, dirpath='./xmls', pdf_name='paper.pdf'):
+    arxiv_info = {}
+    arxiv_info['title'] = result.title
+    arxiv_info['date'] = result.published.strftime('%Y-%m-%d')
+    arxiv_info['time'] = result.published.strftime('%H:%M:%S')
+    arxiv_info['authors'] = [x.name for x in result.authors]
+    arxiv_info['year'] = str(result.published.year)
+    arxiv_info['entry_id'] = str(result.entry_id)
+    arxiv_info['primary_category'] = str(result.primary_category)
+    arxiv_info['categories'] = result.categories
+    arxiv_info['journal_ref'] = str(result.journal_ref)
+    arxiv_info['pdf_url'] = str(result.pdf_url)
+    arxiv_info['doi'] = str(result.doi)
+    arxiv_info['abstract'] = str(result.summary)
 
-    # chatGPT summary
-    dict2 = get_summary(result)
+    os.makedirs(dirpath, exist_ok=True)
 
-    root = {'paper': {**dict, **dict2}}
-    return root
+    pdf_path = os.path.join(dirpath, pdf_name)
+    print('Downloading: ', pdf_path)
+    if DOWNLOAD_PDF:
+        result.download_pdf(dirpath=dirpath, filename=pdf_name)
+    else:
+        print('Skip download.')
+    arxiv_info['pdf'] = pdf_name
 
-def main(query, dir='./xmls', num_papers=3, from_year=2017, max_results=100):
+    loader = PyPDFLoader(os.path.join(dirpath, arxiv_info['pdf']))
+    arxiv_info['pages'] = loader.load_and_split()
+
+    gpt_info = get_gpt_summary(arxiv_info)
+
+    paper_info = {'paper': {**arxiv_info, **gpt_info}}
+    return paper_info
+
+
+def main(query, output_dir='./xmls', num_papers=3, from_year=2017, max_results=100):
     search = arxiv.Search(
         query=query,
         max_results=max_results,
         sort_by=arxiv.SortCriterion.SubmittedDate,
-        sort_order=arxiv.SortOrder.Descending,
+        sort_order=arxiv.SortOrder.Descending
     )
     result_list = []
-    for result in search.results():
-        print(result, result.published.year, result.title)
-        if result.published.year >= from_year:
-            result_list.append(result)
+    for arxiv_info in search.results():
+        if arxiv_info.published.year >= from_year:
+            result_list.append(arxiv_info)
 
     if len(result_list) <= 0:
-        print("#### no result")
+        print('No result')
         sys.exit()
-    
-    if not os.path.exists(dir):  # make subfolder if necessary
-        os.mkdir(dir)
-    
-    results = random.sample(result_list, k=num_papers) if num_papers > 0 and len(result_list) > num_papers else result_list
 
-    for i, result in enumerate(results):
+    # Sample papers if the sample number is less than searched papers
+    results = random.sample(result_list, k=num_papers) if 0 < num_papers < len(
+        result_list) else result_list
+
+    os.makedirs(output_dir, exist_ok=True)
+    for i, arxiv_info in tqdm(enumerate(results), total=len(results)):
         try:
-            id = result.entry_id.replace("http://", "").replace("/", "-")
-            dirpath = f"{dir}/{id}"
-            dict = get_paper_info(result, dirpath=dirpath)
-            dict['paper']['query'] = query
+            print(arxiv_info, arxiv_info.published.year, arxiv_info.title)
+            arxiv_id = arxiv_info.entry_id.replace('http://', '').replace('/', '-')
+            dir_path = os.path.join(output_dir, arxiv_id)
 
-            xml = dicttoxml.dicttoxml(dict, attr_type=False, root=False).decode('utf-8')
-            xml = minidom.parseString(xml).toprettyxml(indent="   ")
-            print("###########\n", xml, "\n#######")
+            # Get paper summary by GPT
+            paper_summary = get_paper_info(arxiv_info, dirpath=dir_path)
 
-            with open(f"{dirpath}/paper.xml", "w") as f:
+            # Save paper info as xml
+            paper_summary['paper']['query'] = query
+            xml = dicttoxml.dicttoxml(paper_summary, attr_type=False, root=False).decode('utf-8')
+            xml = repr(xml)[1:-1]  # Make xml raw string to ignore escape characters which cause parse error
+            xml = minidom.parseString(xml).toprettyxml(indent='   ')
+            xml = minidom.parseString(xml).toprettyxml(indent='   ')
+            xml_path = os.path.join(dir_path, 'paper.xml')
+            with open(xml_path, 'w') as f:
                 f.write(xml)
+                
         except Exception as e:
-            print("Exception", e)
+            print(e)
 
-import argparse
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--year', '-y', type=int, help='from year', default=2017)
-    parser.add_argument('--dir', "-d", type=str, help='destination', default='./xmls')
-    parser.add_argument('--num', "-n", type=int, help='number of papers', default=0)    
+    parser.add_argument('--dir', '-d', type=str, help='destination', default='./xmls')
+    parser.add_argument('--api_key', '-a', type=str, help='OpenAI API key')
+    parser.add_argument('--max_n', '-m', type=int, help='max number of papers', default=100)
+    parser.add_argument('--sample_n', '-n', type=int, help='sample number of papers', default=0)
     parser.add_argument('positional_args', nargs='+', help='query keywords')
     args = parser.parse_args()
 
     print(args)
+    print(f'GPT model: {MODEL_NAME}')
+    print(f'Model temperature: {TEMPERATURE}')
+    print(f'Download PDF: {DOWNLOAD_PDF}')
 
-    main(query=f'all:%22 {" ".join(args.positional_args)} %22', num_papers=args.num, from_year=args.year, dir=args.dir)
+    openai.api_key = args.api_key
+    os.environ['OPENAI_API_KEY'] = openai.api_key
 
+    main(query=f'all:%22 {" ".join(args.positional_args)} %22', num_papers=args.sample_n, from_year=args.year,
+         output_dir=args.dir, max_results=args.max_n)
